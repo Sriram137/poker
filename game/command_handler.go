62dc616b0631af0c3d73cc1af9d1fd135d4aae17
Taha <taha.j@directi.com>
2014-05-29 18:57:45 +0530

    Fixes #12 (Prettyifying ls command)

--- game/command_handler.go
+++ game/command_handler.go
@@ -47,12 +47,12 @@ func getPlayerPositionsInPrintableFormat(pokerBoard *board.Board) string {
 		if currPlayer.Folded == true {
 			playerPosString = playerPosString + "(f) "
 		}
-		playerPosString = playerPosString + "bet:" + strconv.Itoa(currPlayer.CurrentBet) + "money :" + strconv.Itoa(currPlayer.Money) + " "
+		playerPosString = playerPosString + "bet:" + strconv.Itoa(currPlayer.CurrentBet) + " money:" + strconv.Itoa(currPlayer.Money) + " "
 		if currPlayer == pokerBoard.Starter {
-			playerPosString = "***" + playerPosString
+			playerPosString = "**" + playerPosString
 		}
 		if currPlayer.Next_player != pokerBoard.CurrentPlayer {
-			playerPosString = playerPosString + "-> "
+			playerPosString = playerPosString + "--> "
 		}
 		currPlayer = currPlayer.Next_player
 		if currPlayer == pokerBoard.CurrentPlayer {
