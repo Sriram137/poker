d0bfd108c6c8b646dd093a1632ac88b619debe5c
Harishchandra Reddy <harish.7000@gmail.com>
2014-05-29 15:22:57 +0530

    find best hand out of 7 complete

--- tools/ranking.go
+++ tools/ranking.go
@@ -1,52 +1,148 @@
-package tools
+package ranking
 
-func rank(cards []int) ([]int, []int) {
-	freqs := make([]int, 13)
-	out1 := make([]int, 5)
-	out2 := make([]int, 5)
-	i1 := 0
-	i2 := 0
-	for _, v := range cards {
-		freqs[12-v+2]++
-	}
-	for i, v := range freqs {
-		if v == 4 {
-			out1[i1] = v
-			out2[i2] = 14 - i
-			i1++
-			i2++
+import (
+	"sort"
+)
+
+func card_ranks(hand [5]string) ([5]int){
+	var ranks [5]int
+	rankSpecial := [5]int{14, 5, 4, 3, 2}
+	for i, v := range hand {
+	 	if (v[0] == 'A') {
+			ranks[i] = 14
+		} else if (v[0] == 'K') {
+			ranks[i] = 13
+		} else if (v[0] == 'Q') {
+			ranks[i] = 12
+		} else if (v[0] == 'J') {
+			ranks[i] = 11
+		} else if (v[0] == 'T') {
+			ranks[i] = 10
+		} else {
+			ranks[i] = int(v[0] - '0')
 		}
 	}
-	for i, v := range freqs {
-		if v == 3 {
-			out1[i1] = v
-			out2[i2] = 14 - i
-			i1++
-			i2++
-		}
+	sort.Sort(sort.Reverse(sort.IntSlice(ranks[0:5])))
+	if (ranks  == rankSpecial) {
+		ranks = [5]int{5, 4, 3, 2, 1}
 	}
-	for i, v := range freqs {
-		if v == 2 {
-			out1[i1] = v
-			out2[i2] = 14 - i
-			i1++
-			i2++
-		}
-	}
-	for i, v := range freqs {
-		if v == 1 {
-			out1[i1] = v
-			out2[i2] = 14 - i
-			i1++
-			i2++
-		}
-	}
-	return out1, out2
+	return ranks
 }
 
-func findRank(cards []int) ([]int, int) {
-	maxHand := make([]int, 5)
-	maxRank := 0
+func hand_rank(hand [5]string) ([]int) {
+	ranks := card_ranks(hand)
+	var ret []int	
+	if (straight(ranks) && flush(hand)) {
+		ret = append(ret, 8, max(ranks))
+		return ret
+	} else if (kind(4, ranks) != -1) {
+		ret = append(ret, 7, kind(4, ranks), kind(1, ranks))
+		return ret
+	} else if (kind(3, ranks) != -1 && kind(2, ranks) != -1) {
+		ret = append(ret, 6, kind(3, ranks), kind(2, ranks))
+		return ret
+	} else if (flush(hand)) {
+		ret = append(ret, 5)
+		ret = append(ret, ranks[0:5]...)
+		return ret
+	} else if (straight(ranks)) {
+		ret = append(ret, 4, max(ranks))
+		return ret
+	} else if (kind(3, ranks) != -1) {
+		ret = append(ret, 3, kind(3, ranks))
+		ret = append(ret, ranks[0:5]...)
+		return ret
+	} else if a,b := two_pairs(ranks); a != -1 && b != -1 {
+		a, b = two_pairs(ranks)
+		ret = append(ret, 2, a, b)
+		ret = append(ret, ranks[0:5]...)
+		return ret
+	} else if (kind(2, ranks) != -1) {
+		ret = append(ret, 1, kind(2, ranks))
+		ret = append(ret, ranks[0:5]...)
+		return ret
+	} else {
+		ret = append(ret, 0)
+		ret = append(ret, ranks[0:5]...)
+		return ret
+	}
+}
+
+func max(ranks [5]int) (int) {
+	m := 0
+	for i, v := range ranks {
+		if (i == 0) {
+			m = v
+		} else  {
+			if v > m {
+				m = v
+			}
+		}
+	}
+	return m
+}
+
+func straight(ranks [5]int) (bool) {
+	len := len(ranks)
+	if (ranks[0] - ranks[4] == 4 && len == 5) {
+		return true
+	} else {
+		return false
+	}
+}
+
+func two_pairs(ranks [5]int) (int, int) {
+	pair := kind(2, ranks)
+	sort.Sort(sort.IntSlice(ranks[0:5]))
+	lowpair := kind(2, ranks)
+	if (pair != -1 && pair != lowpair) {
+		return pair, lowpair
+	} else {
+		return -1, -1
+	}
+}
+
+func flush(hand [5]string) (bool) {
+	var suit string
+	count := 0
+	for i, v := range hand {
+		if (i == 0) {
+			suit = string(v[1])
+			count = 1
+		} else {
+			if (string(v[1]) == suit) {
+				count++
+			}
+		}
+	}
+	if (count == 5) {
+		return true
+	} else {
+		return false
+	}
+}
+
+func kind(n int, ranks [5]int) (rank int) {
+	for _, v := range ranks {
+		if (count(v, ranks) == n) {
+			return v
+		}
+	}
+	return -1
+}
+
+func count(r int, ranks [5]int) (int) {
+	count := 0
+	for _, v := range ranks {
+		if (v == r) {
+			count++
+		}
+	}
+	return count
+}
+
+func findBestHand(cards []int) []int {
+	maxHand := make([]int, 10)
 	for i := 0; i < 6; i++ {
 		for j := i + 1; j < 7; j++ {
 			temp1 := make([]int, i)
@@ -55,17 +151,28 @@ func findRank(cards []int) ([]int, int) {
 			copy(temp1, cards[:i])
 			copy(temp2, cards[i+1:j])
 			copy(temp3, cards[j+1:])
-			curHand := append(append(temp1, temp2...), temp3...)
-			curRank := dummy(curHand)
-			if curRank > maxRank {
-				maxRank = curRank
+			curHand := append(append(temp1, temp2...), temp3...)			
+			if compareHands(curRank, maxRank) > 0 {		
 				maxHand = curHand
 			}
 		}
 	}
-	return maxHand, maxRank
+	return maxHand
 }
 
-func dummy(cards []int) int {
-	return cards[4]
+func compareHands(firstHand []int, secondHand []int) int {
+	for i, v := range firstHand {
+		if v > secondHand[i] {
+			return 1
+		}
+		if v < secondHand[i] {
+			return -1
+		}
+	}
+	return 0
+}
+
+func test () {
+	h = [5]string{"2S", "2D", "2C", "2E", "5S"}
+	ret := hand_rank(h)
 }
